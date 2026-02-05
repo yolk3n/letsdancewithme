026951dacc786bcc971a83036e03fae5c5995673
yolk3n <kalexndr@mail.ru>
2026-02-05 22:00:13 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const Database = require("better-sqlite3");
 
-const db = new Database("database.db");
+const path = require("path");
+const Database = require("better-sqlite3");
+
+const dbPath = path.join(__dirname, "database.db");
+const db = new Database(dbPath);
 
 // Таблица пользователей
 db.prepare(`
